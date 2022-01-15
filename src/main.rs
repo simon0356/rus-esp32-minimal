1e864eaf55dd93e1dde6b40c1354cb72e712dd5b
simon <Simon Trope>
2022-01-15 17:07:40 +0100

    initial commit
    Signed-off-by: simon <Simon Tropée>

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use esp_idf_hal::prelude::*;
+use esp_idf_svc::sysloop::*;
+use std::{thread, time::Duration};
+
+fn main() -> anyhow::Result<()> {
+    loop {
+        println!("Hello world");
+        thread::sleep(Duration::from_secs(1));
+    }
+}
